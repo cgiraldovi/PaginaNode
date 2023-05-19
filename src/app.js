3261b17528da994a067fdbe80224bd88c8cf1da2
cgiraldovi <72058741+cgiraldovi@users.noreply.github.com>
2023-05-19 17:36:52 -0500

    Segundo commit, configuraciones basicas

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 require('colors');
+require('dotenv').config();
 const express = require('express');
+
 const app = express();
 
 //genera las rutas
@@ -14,6 +16,6 @@ app.use(express.static("statics/html")); //implementaremos middleware para una p
 
 
 //genera el servidor corriendo en el puerto 5000
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
     console.log("servidor corriendo desde express".rainbow)
 })
